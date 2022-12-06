d87fec7424cc3610be2d8ec417b85a08cd72d7f0
Kathryn Hackman <kathrynhackman@kathryns-mbp.myfiosgateway.com>
2022-12-05 22:35:26 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      'retro-red': '#6F263D',
+      'powder-blue': '#6BACE4'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
